c78f5d84805318daabd2ac28ed17e8fb2e625f4d
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-12-08 23:07:22 +0600

    Nothing to commit

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ const NotFoundPage = () => {
     return (
         <div className="text-center min-h-screen flex flex-col justify-center items-center">
             <h2 className="text-6xl font-medium">404</h2>
-            <p>Oops!! Page Not Found</p>
+            <p>Oops!! The page you are looking for was not found</p>
         </div>
     );
 };
